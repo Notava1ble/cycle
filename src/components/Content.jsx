b9d3e507f768559fb5b1078086c8d39dfe21831f
Visar <visarmullaj07@gmail.com>
2024-04-21 17:38:13 +0200

    finally finished

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,22 +2,24 @@ import WorkData from "../data/worksData";
 
 const Content = () => {
 	return (
-		<div className="w-full p-6  grid grid-cols-3 gap-6">
+		<div className="w-full p-6 grid lg:grid-cols-3 md:grid-cols-2 gap-y-6 gap-10">
 			{WorkData.map((item) => (
 				<div
 					key={item.id}
-					className="bg-green-500 text-white p-6 rounded-xl overflow-hidden transition-all hover:scale-105 cart-shadow"
+					className="lg:h-[450px] md:h-[500px] h-[550px] bg-green-500 text-white rounded-xl overflow-hidden transition-all hover:scale-105 cart-shadow"
 				>
-					<img
-						src={item.img}
-						alt={item.title}
-						className="w-full border aspect-video"
-					/>
-					<div>
-						<h1 className="font-poppins text-2xl font-bold mt-6 mb-2">
-							{item.title}
-						</h1>
-						<p className=" ">{item.description}</p>
+					<div
+						className="w-full h-full bg-cover bg-no-repeat bg-center innter-shadow"
+						style={{ backgroundImage: `url(${item.img}) ` }}
+					>
+						<div className="flex flex-col justify-end bg-gradient-to-b from-transparent to-gray-500 p-6 h-full">
+							<div className="w-full sm:h-[30%] md:h-[40%]">
+								<h1 className="font-poppins text-2xl font-bold mb-2 text-shadow">
+									{item.title}
+								</h1>
+								<p className="text-shadow text-small">{item.description}</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			))}
--- src/data/worksData.js
+++ src/data/worksData.js
@@ -1,61 +1,92 @@
-import image from "../assets/image.png";
+import arke from "../assets/images/arke.jpeg";
+import bottle from "../assets/images/bottle.jpeg";
+import football from "../assets/images/football.jpeg";
+import luge from "../assets/images/luge.jpeg";
+import lule from "../assets/images/lule.jpeg";
+import poster from "../assets/images/poster.jpeg";
+import shkaterrim from "../assets/images/shkaterrim.jpeg";
+import trung from "../assets/images/trung.jpeg";
+import vazo from "../assets/images/vazo.jpeg";
+import vazotjt from "../assets/images/vazotjt.jpeg";
+import vizatim from "../assets/images/vizatim.jpeg";
 
 const workData = [
 	{
-		title: "This is a Title",
+		title: "Shporte",
 		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed ex pulvinar, aliquet mauris et, bibendum risus. Maecenas tincidunt accumsan turpis, eu sagittis tortor facilisis nec. Duis efficitur neque sodales lorem mattis faucibus. In hac habitasse platea dictumst. Pellentesque vitae mattis sem. Quisque vitae metus placerat, dictum urna at, tincidunt leo. Nullam imperdiet interdum lectus, id ullamcorper erat placerat sit amet. Quisque tempus et neque id tristique. Curabitur porttitor vulputate velit, sed sodales risus malesuada quis. Nullam in hendrerit neque.",
-		img: image,
+			"Një shportë të vogël, e punuar me detaje të ndërlikuara, e cila përmban lule delikate dhe artikuj dekorativë. Kjo tregon bukurinë e thjeshtë por elegante me dore.",
+		img: arke,
 		id: 1,
 	},
 	{
-		title: "This is a Title",
+		title: "Shishe Dekorative",
 		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed ex pulvinar, aliquet mauris et, bibendum risus. Maecenas tincidunt accumsan turpis, eu sagittis tortor facilisis nec. Duis efficitur neque sodales lorem mattis faucibus. In hac habitasse platea dictumst. Pellentesque vitae mattis sem. Quisque vitae metus placerat, dictum urna at, tincidunt leo. Nullam imperdiet interdum lectus, id ullamcorper erat placerat sit amet. Quisque tempus et neque id tristique. Curabitur porttitor vulputate velit, sed sodales risus malesuada quis. Nullam in hendrerit neque.",
-		img: image,
+			"Kjo fotografi tregon një shishe të pikturuar me një sfond blu dhe një figurë te zeze dhe te bardhë, duke kapur thelbin e artit në mjediset e përditshme.",
+		img: bottle,
 		id: 2,
 	},
 	{
-		title: "This is a Title",
+		title: "Tavoline Futbolli",
 		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed ex pulvinar, aliquet mauris et, bibendum risus. Maecenas tincidunt accumsan turpis, eu sagittis tortor facilisis nec. Duis efficitur neque sodales lorem mattis faucibus. In hac habitasse platea dictumst. Pellentesque vitae mattis sem. Quisque vitae metus placerat, dictum urna at, tincidunt leo. Nullam imperdiet interdum lectus, id ullamcorper erat placerat sit amet. Quisque tempus et neque id tristique. Curabitur porttitor vulputate velit, sed sodales risus malesuada quis. Nullam in hendrerit neque.",
-		img: image,
+			"Kjo eshtë një lojë futbolli tavoline e bërë me dorë, është një krijim shumë interesant dhe argëtues. Këto lloj projektesh janë të mira jo vetëm për riciklimin, por edhe për të inkurajuar lojën kreative.",
+		img: football,
 		id: 3,
 	},
 	{
-		title: "This is a Title",
+		title: "Lule",
 		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed ex pulvinar, aliquet mauris et, bibendum risus. Maecenas tincidunt accumsan turpis, eu sagittis tortor facilisis nec. Duis efficitur neque sodales lorem mattis faucibus. In hac habitasse platea dictumst. Pellentesque vitae mattis sem. Quisque vitae metus placerat, dictum urna at, tincidunt leo. Nullam imperdiet interdum lectus, id ullamcorper erat placerat sit amet. Quisque tempus et neque id tristique. Curabitur porttitor vulputate velit, sed sodales risus malesuada quis. Nullam in hendrerit neque.",
-		img: image,
+			"Kjo fotografi tregon një prezantim kreativ të luleve të rrethuara nga lugë, duke shfaqur një ekspozitë artistike dhe jo tradicionale që është vizualisht tërheqëse.",
+		img: luge,
 		id: 4,
 	},
 	{
-		title: "This is a Title",
+		title: "Peme fotosh",
 		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed ex pulvinar, aliquet mauris et, bibendum risus. Maecenas tincidunt accumsan turpis, eu sagittis tortor facilisis nec. Duis efficitur neque sodales lorem mattis faucibus. In hac habitasse platea dictumst. Pellentesque vitae mattis sem. Quisque vitae metus placerat, dictum urna at, tincidunt leo. Nullam imperdiet interdum lectus, id ullamcorper erat placerat sit amet. Quisque tempus et neque id tristique. Curabitur porttitor vulputate velit, sed sodales risus malesuada quis. Nullam in hendrerit neque.",
-		img: image,
+			"Një ekspozitë krijuese me foto te ndyshme të bashkangjitura në degët e një peme të vogël me gjethe të gjelbërta, e vendosur në një vazo me ngjyra, duke shfaqur një dekorim artistik dhe personalizuar.",
+		img: lule,
 		id: 5,
 	},
 	{
-		title: "This is a Title",
+		title: "Koha po Mbaron",
 		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed ex pulvinar, aliquet mauris et, bibendum risus. Maecenas tincidunt accumsan turpis, eu sagittis tortor facilisis nec. Duis efficitur neque sodales lorem mattis faucibus. In hac habitasse platea dictumst. Pellentesque vitae mattis sem. Quisque vitae metus placerat, dictum urna at, tincidunt leo. Nullam imperdiet interdum lectus, id ullamcorper erat placerat sit amet. Quisque tempus et neque id tristique. Curabitur porttitor vulputate velit, sed sodales risus malesuada quis. Nullam in hendrerit neque.",
-		img: image,
+			"Një vizatim që kombinon në mënyrë krijuese ne një ore rere Tokën me një peizazh urban, të rrethuar nga elemente të ndryshme; ajo përcjell një mesazh për urgjencën e adresimit të çështjeve mjedisore",
+		img: poster,
 		id: 6,
 	},
 	{
-		title: "This is a Title",
+		title: "Shkaterrim",
 		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed ex pulvinar, aliquet mauris et, bibendum risus. Maecenas tincidunt accumsan turpis, eu sagittis tortor facilisis nec. Duis efficitur neque sodales lorem mattis faucibus. In hac habitasse platea dictumst. Pellentesque vitae mattis sem. Quisque vitae metus placerat, dictum urna at, tincidunt leo. Nullam imperdiet interdum lectus, id ullamcorper erat placerat sit amet. Quisque tempus et neque id tristique. Curabitur porttitor vulputate velit, sed sodales risus malesuada quis. Nullam in hendrerit neque.",
-		img: image,
+			"Ne kete vizatim paraqitet nje peme qe po shkaterrohet dita-dites na keqtrajtimi i njeriut.",
+		img: shkaterrim,
 		id: 7,
 	},
 	{
-		title: "This is a Title",
+		title: "Mesazhi i Drurit",
 		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed ex pulvinar, aliquet mauris et, bibendum risus. Maecenas tincidunt accumsan turpis, eu sagittis tortor facilisis nec. Duis efficitur neque sodales lorem mattis faucibus. In hac habitasse platea dictumst. Pellentesque vitae mattis sem. Quisque vitae metus placerat, dictum urna at, tincidunt leo. Nullam imperdiet interdum lectus, id ullamcorper erat placerat sit amet. Quisque tempus et neque id tristique. Curabitur porttitor vulputate velit, sed sodales risus malesuada quis. Nullam in hendrerit neque.",
-		img: image,
+			"Një pllakë druri me fjalët “MBRO TOKEN”, të shoqëruara nga një enë qelqi e vogël me bimë të gjelbërta, nje pune interesante për shkak të kontrastit të elementeve natyrore dhe artificiale",
+		img: trung,
 		id: 8,
+	},
+	{
+		title: "Vazo",
+		description:
+			"Kjo fotografi tregon një projektim të bukur të luleve dhe dekorimeve prej letre në një enë me ngjyrë rozë të mermerizuar, duke kapur thelbin e kreativitetit dhe estetikës.",
+		img: vazo,
+		id: 9,
+	},
+	{
+		title: "Shkëlqimi i Artë",
+		description:
+			"Nje dekorim i ngjashëm me një vazo të artë të, i cili tërheq vëmendjen për shkak të pamjes së saj të pazakontë dhe të ndritshme.",
+		img: vazotjt,
+		id: 10,
+	},
+	{
+		title: "Vizatim",
+		description:
+			"Kjo fotografi paraqet një pikturë që tregon një shishe të vendosur kundër një sfondi me lule të verdha të ndezura, bar të gjelbër, dhe një qielli blu me re; ajo kap thelbin e bukurisë së natyrës të shkaterruar nja krijimtaria njerzore. ",
+		img: vizatim,
+		id: 11,
 	},
 ];
 
